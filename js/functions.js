1eed81968d7a4edd123578ea804733f1b6202fb7
marorcas <ortzmc@gmail.com>
2024-06-26 12:22:56 +1000

    Add task view icon on menubar

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+function hoverTask(element) {
+    element.setAttribute('src', './assets/node_modules/bootstrap-icons/icons/view-stacked.svg');
+}
+  
+function unhoverTask(element) {
+    element.setAttribute('src', './assets/node_modules/bootstrap-icons/icons/view-list.svg');
+}
